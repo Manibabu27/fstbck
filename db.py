185898b609bd440da3606a1bcb649755a6148548
Manibabu2710 <psnaidu98765@gmail.com>
2026-02-23 20:27:08 +0530

    envi

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
 
+import os
+# from urllib.parse import quote_plus
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
-from urllib.parse import quote_plus
 
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("No DATABASE_URL found in environment variables")
 
 # host = 'localhost'
 # port = 5432
@@ -20,9 +25,10 @@ from urllib.parse import quote_plus
 # password_enc =quote_plus(password)
 
 # conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-# dblink = f"postgresql://{user_enc}:{password_enc}@{host}/{dbname}"
-dblink = "postgres:qkqPoMWCMRWGAhtvKQcnSmOMWWTCqfeB@postgres.railway.internal:5432/railway"
-engine = create_engine(dblink)
+# DATABASE_URL= f"postgresql://{user_enc}:{password_enc}@{host}/{dbname}"
+
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 
 Base = declarative_base()
